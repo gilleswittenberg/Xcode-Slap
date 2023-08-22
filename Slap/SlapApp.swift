35284eeb7b8284360896696d52353ba0c2dadd71
Gilles Wittenberg <mail@gilleswittenberg.com>
2023-08-22 23:24:06 +0200

    Initial Commit

---
+++ Slap/SlapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlapApp.swift
+//  Slap
+//
+//  Created by Gilles Wittenberg on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
