2a96d5dbc83ff6a53009b55420e4114116810a74
Gilles Wittenberg <mail@gilleswittenberg.com>
2023-09-21 16:49:45 +0200

    Instant, Average, Weighted algorithms

--- Slap/Clock.swift
+++ Slap/Clock.swift
@@ -7,6 +7,41 @@
 
 import Foundation
 
+fileprivate enum AverageAlgorithm {
+    case instant, average, weighted
+}
+
+fileprivate func averageDuration (_ durations: [Duration]) -> Duration {
+    let sum = durations.reduce(.seconds(0), +)
+    return sum / durations.count
+}
+
+fileprivate func averageWeightedDuration (_ durations: [Duration]) -> Duration {
+    let sum = durations.enumerated().reduce(Duration.seconds(0), { (accumulate, current) in
+        let index = current.0
+        let value = current.1
+        let multiplier: Double = 1 + Double(index) * 0.2
+        return accumulate + value * multiplier
+    })
+    let denominator = durations.enumerated().reduce(0, { (accumulate, current) in
+        let index = current.0
+        let multiplier = 1 + Double(index) * 0.2
+        return accumulate + multiplier
+    })
+    return sum / denominator
+}
+
+fileprivate func calculateAverageDuration (_ durations: [Duration], _ algorithm: AverageAlgorithm) -> Duration {
+    switch algorithm {
+    case .instant:
+        return averageDuration(durations)
+    case .average:
+        return averageDuration(durations)
+    case .weighted:
+        return averageWeightedDuration(durations)
+    }
+}
+
 fileprivate func diffClockInstants (_ taps: [SuspendingClock.Instant]) -> [Duration] {
     var diffs: [Duration] = []
     for (index, tap) in taps.enumerated() {
@@ -22,14 +57,20 @@ fileprivate func filterDurations (_ durations: [Duration]) -> [Duration] {
     durations.filter { d in d > .seconds(0) }
 }
 
-fileprivate func averageDuration (_ durations: [Duration]) -> Duration {
-    let sum = durations.reduce(.seconds(0), +)
-    return sum / durations.count
+fileprivate func lastTaps (_ taps: [SuspendingClock.Instant], _ algorithm: AverageAlgorithm) -> [SuspendingClock.Instant] {
+    switch algorithm {
+    case .instant:
+        return Array(taps.suffix(2))
+    case .average:
+        return taps
+    case .weighted:
+        return Array(taps.suffix(6))
+    }
 }
 
-fileprivate func calculateBPM (_ taps: [SuspendingClock.Instant]) -> Double? {
-    let lastTaps = Array(taps.suffix(6))
-    let durations = filterDurations(diffClockInstants(lastTaps))
+fileprivate func calculateBPM (_ allTaps: [SuspendingClock.Instant], algorithm: AverageAlgorithm = .instant) -> Double? {
+    let taps = lastTaps(allTaps, algorithm)
+    let durations = filterDurations(diffClockInstants(taps))
     guard durations.isEmpty == false else { return nil }
     let d = averageDuration(durations)
     guard d > .seconds(0) else { return nil }
@@ -50,11 +91,12 @@ class Clock: ObservableObject {
     }
     
     private var taps: [SuspendingClock.Instant] = []
+    private var algorithm = AverageAlgorithm.weighted
     private var timer: Timer?
     
     func appendTap () {
         taps.append(clock.now)
-        BPM = calculateBPM(taps)
+        BPM = calculateBPM(taps, algorithm: algorithm)
         scheduleTimer()
     }
     
